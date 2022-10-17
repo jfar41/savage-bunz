b521cb8f5a669875f3fe6b5694e63e21ba96ad5c
jfar41 <68749301+jfar41@users.noreply.github.com>
2022-10-17 11:19:41 -0700

    Basic layout for header + landing

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import "./app.scss";
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <img src={require("./images/sb-logo.png")} className="logo"/>
+            
+        </div>
+    )
+}
---
+++ src/pages/Landing.js
@@ -0,0 +1,12 @@
+import "./pages.scss";
+
+export const Landing = () => {
+    return (
+        <div className="pages">
+            <div className="landing">
+                <img src={require("../images/landing/savage1.jpg")} className="slideshow" />
+
+            </div>
+        </div>
+    )
+}
